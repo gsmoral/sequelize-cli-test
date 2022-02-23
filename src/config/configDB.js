d62d756ba95d65029716f84402b9fe22a190e799
PC20-GUILLER\Guiller <gsmoral@gmail.com>
2022-02-23 15:55:29 +0100

    Init

---
+++ src/config/configDB.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "development": {
+      username: 'postgres',
+      password: 'P0stgr3s',
+      database: 'database_development',
+      host: '127.0.0.1',
+      port: 5432,
+      dialect: 'postgres'
+  },
+  "test": {
+      username: process.env.CI_DB_USERNAME,
+      password: process.env.CI_DB_PASSWORD,
+      database: 'database_test',
+      host: '127.0.0.1',
+      port: 5432,
+      dialect: 'postgres'
+  },
+  "production": {
+      username: process.env.PROD_DB_USERNAME,
+      password: process.env.PROD_DB_PASSWORD,
+      database: process.env.PROD_DB_NAME,
+      host: process.env.PROD_DB_HOSTNAME,
+      port: process.env.PROD_DB_PORT,
+      dialect: 'postgres'
+  }
+};
