7f0a082e16b57e670618b0f1caebf4f5199c5f52
PC20-GUILLER\Guiller <gsmoral@gmail.com>
2022-02-23 17:53:44 +0100

    Test2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log('Hello World!')
+const db = require('./models');
+const express = require('express');
+
+function getTasksPromise() {
+  db.User.findAll()
+    .then((users) => {
+      console.log('USER', users);
+      users.forEach((user) => {
+        console.log('NAME: ', user.firstName);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+// const users = getTasksPromise();
+
+const app = express();
+
+// Asignamos puerto de variable de entorno, si no existe ponemos 3000
+const port = process.env.PORT || 3005;
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+// Iniciamos el servidor
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
